3852ceef3d046f5932eb970dc55934616bccb31b
Christopher <chris09332@gmail.com>
2025-11-26 11:01:17 +0000

    Started on one sort method, will try either use quicksort or mergesort I think

--- src/main/java/utility/Sort.java
+++ src/main/java/utility/Sort.java
@@ -1,4 +1,28 @@
 package utility;
 
-public final class Sort {
+import model.linkedlist.LinkedList;
+
+import java.util.Comparator;
+
+public final class Sort{
+    public static <E> LinkedList<E> sortAscending(LinkedList<E> list, Comparator<E> comparator){
+        if(list.size() < 2)
+            return list;
+        if(list.size() == 2){
+            int index = comparator.compare(list.get(0), list.get(1));
+            if(index == 0){
+                return list;
+            }else if(index > 0){
+                E old = list.get(0);
+                list.set(0, list.get(1));
+                list.set(1, old);
+                return list;
+            }else{
+                return list;
+            }
+        }
+        LinkedList<E> one = new LinkedList<>(), two = new LinkedList<>();
+        int mean = 0;
+        return null;
+    }
 }
