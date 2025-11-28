2eef0ee138e253efb1ca78474d3d0fb8572e2fc3
Christopher <chris09332@gmail.com>
2025-11-28 09:23:25 +0000

    Implemented Merge Sort

--- src/main/java/utility/Sort.java
+++ src/main/java/utility/Sort.java
@@ -5,24 +5,23 @@ import model.linkedlist.LinkedList;
 import java.util.Comparator;
 
 public final class Sort{
-    public static <E> LinkedList<E> sortAscending(LinkedList<E> list, Comparator<E> comparator){
+    public static <E> void mergeSort(LinkedList<E> list, Comparator<E> comparator){
         if(list.size() < 2)
-            return list;
-        if(list.size() == 2){
-            int index = comparator.compare(list.get(0), list.get(1));
-            if(index == 0){
-                return list;
-            }else if(index > 0){
-                E old = list.get(0);
-                list.set(0, list.get(1));
-                list.set(1, old);
-                return list;
-            }else{
-                return list;
-            }
-        }
+            return;
+
+        int size = list.size();
         LinkedList<E> one = new LinkedList<>(), two = new LinkedList<>();
-        int mean = 0;
-        return null;
+        one.addAll(list.subList(0, size/2));
+        two.addAll(list.subList(size/2, size));
+        mergeSort(one, comparator);
+        mergeSort(two, comparator);
+
+        int index = 0,oneCount = 0, twoCount = 0, oneSize = one.size(), twoSize = two.size();
+        while(oneCount < oneSize && twoCount < twoSize){
+            E item = comparator.compare(one.get(oneCount), two.get(twoCount)) < 0 ? one.get(oneCount++) : two.get(twoCount++);
+            list.set(index++, item);
+        }
+        while(oneCount < oneSize) list.set(index++, one.get(oneCount++));
+        while(twoCount < twoSize) list.set(index++, two.get(twoCount++));
     }
 }
