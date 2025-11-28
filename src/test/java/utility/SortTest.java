3cc3292afe70c355db1eed41603876f628ccf33c
Christopher <chris09332@gmail.com>
2025-11-28 09:23:36 +0000

    Tested Merge Sort

---
+++ src/test/java/utility/SortTest.java
@@ -0,0 +1,46 @@
+package utility;
+
+import model.linkedlist.LinkedList;
+
+import org.junit.jupiter.api.*;
+import java.util.Comparator;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortTest {
+    LinkedList<String> strings;
+    @BeforeEach
+    void setUp() {
+        strings = new LinkedList<>();
+        strings.add("B");
+        strings.add("A");
+        strings.add("C");
+        strings.add("F");
+        strings.add("D");
+    }
+
+    @AfterEach
+    void tearDown() {
+        strings = null;
+    }
+
+    @Nested
+    class mergeSort {
+        @Test
+        void sortAscending() {
+            int size = strings.size();
+            Sort.mergeSort(strings, String::compareTo);
+            assertEquals(size, strings.size());
+            assertEquals("A", strings.getFirst());
+            assertEquals("F", strings.getLast());
+        }
+
+        @Test
+        void sortDescending() {
+            int size = strings.size();
+            Sort.mergeSort(strings, Comparator.reverseOrder());
+            assertEquals(size, strings.size());
+            assertEquals("F", strings.getFirst());
+            assertEquals("A", strings.getLast());
+        }
+    }
+}
