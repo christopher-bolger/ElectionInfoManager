bfc0a29cb5c9e35610895392d95343670fd0a4b2
Christopher <chris09332@gmail.com>
2025-12-01 10:59:46 +0000

    initial commit

---
+++ src/test/java/model/election/ElectionTest.java
@@ -0,0 +1,46 @@
+package model.election;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElectionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void sortByVotes() {
+    }
+
+    @Test
+    void getCandidates() {
+    }
+
+    @Test
+    void getWinners() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void getAffiliation() {
+    }
+
+    @Test
+    void getPolitician() {
+    }
+}
