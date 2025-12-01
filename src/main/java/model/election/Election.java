6764f6dda0899a1e6006c42ffe996e54480c78b7
Christopher <chris09332@gmail.com>
2025-12-01 10:59:07 +0000

    Added method for finding winners & a couple of getters

--- src/main/java/model/election/Election.java
+++ src/main/java/model/election/Election.java
@@ -4,23 +4,32 @@ import model.hashmap.CustomHashMap;
 import model.linkedlist.LinkedList;
 import utility.Sort;
 
+import java.time.LocalDate;
 import java.util.Comparator;
+import java.util.Date;
 
 public class Election {
     public final static String[] ELECTION_TYPES = {"General", "Local", "European", "Presidential"};
-    int electionType, electionYear, totalWinners;
+    int electionType, totalWinners;
+    LocalDate date;
     String electionLocation;
     LinkedList<ElectionEntry> politicians;
-    CustomHashMap<Integer, ElectionEntry> candidates;
+    CustomHashMap<Politician, ElectionEntry> candidates;
 
-    public Election(int type, int year, int winners, String location) {
+    public Election(int type, LocalDate date, int winners, String location) {
         electionType = Math.max(type, 0);
-        electionYear = Math.max(year, 0);
         totalWinners = Math.max(winners, 1);
 
+        if(date != null)
+            this.date = date;
+        else this.date = LocalDate.now();
+
         if(location != null && !location.isEmpty())
-            electionLocation = location;
-        else electionLocation = "INVALID";
+            for(int i = 0; i < Politician.COUNTIES.length; i++)
+                if(Politician.COUNTIES[i].equals(location))
+                    electionLocation = Politician.COUNTIES[i];
+        if(electionLocation != null && electionLocation.isEmpty())
+            electionLocation = "INVALID";
 
         politicians = new LinkedList<>();
         candidates = new CustomHashMap<>();
@@ -30,14 +39,81 @@ public class Election {
         Sort.mergeSort(politicians, Comparator.comparingInt(ElectionEntry::getVotes));
     }
 
+    public void sortByAffiliation(){
+        Sort.mergeSort(politicians, Comparator.comparing(ElectionEntry::getAffiliation));
+    }
+
+    public LinkedList<ElectionEntry> getCandidates() {
+        return politicians;
+    }
+
+    private int getQuota(){ //Droop formula
+        int totalVotes = 0;
+        for(ElectionEntry e : politicians)
+            totalVotes += e.getVotes();
+        return (totalVotes / (totalWinners + 1)) + 1;
+    }
+
+    public LinkedList<ElectionEntry> getWinners(){
+        LinkedList<ElectionEntry> winners = new LinkedList<>();
+        return switch(electionType) {
+            case 0, 1, 2 -> {
+                int quota = getQuota();
+                switch(electionType){
+                    case 0 -> sortByAffiliation();
+                    case 1, 2 -> sortByVotes();
+                }
+                for (ElectionEntry e : politicians) //i'd use a while loop if I had vote preferencing but since I don't I could get stuck in the loop
+                    if (e.getVotes() > quota)
+                        winners.add(e);
+                yield winners;
+            }
+            case 3 -> {
+                sortByVotes();
+                winners.add(politicians.getFirst());
+                yield winners;
+            }
+            default -> null;
+        };
+    }
+
     public void add(Politician politician){
         politicians.add(new ElectionEntry(politician));
+        candidates.put(politician, new ElectionEntry(politician));
     }
 
     public void add(Politician politician, int votes){
         politicians.add(new ElectionEntry(politician, votes));
+        candidates.put(politician, new ElectionEntry(politician));
+        candidates.get(politician).setVotes(votes);
     }
 
+    public void remove(Politician politician){
+        politicians.remove(candidates.get(politician));
+        candidates.remove(politician);
+    }
 
+    public String getAffiliation(Politician politician){
+        return candidates.get(politician).getAffiliation();
+    }
 
+    public Politician getPolitician(ElectionEntry electionEntry){
+        return candidates.get(electionEntry.getPolitician()).getPolitician();
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
+
+    public String getElectionType(){
+        return ELECTION_TYPES[electionType];
+    }
+
+    public String getElectionLocation(){
+        return electionLocation;
+    }
+
+    public int hashCode(){
+        return date.getYear() + (date.getMonthValue()*100 + date.getMonthValue() )+ date.getDayOfMonth();
+    }
 }
